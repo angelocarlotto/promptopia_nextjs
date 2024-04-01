98d108498798c4384039403b4c2113ed4d775228
Angelo Carlotto <angelocarlotto@gmail.com>
2024-03-31 22:05:06 -0400

    impemented search and click tag

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -19,13 +19,43 @@ const PromptCardList = ({ data, handleTagClick }) => {
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
   const [allPosts, setAllPosts] = useState([]);
-  const handleSearchChange = (e) => {};
+  const [searchedTimeOut, setSearchedTimeOut] = useState(null);
+  const [serchedResults, setSearchedResults] = useState([]);
+  const handleSearchChange = async (e) => {
+    clearTimeout(searchedTimeOut);
+    setSearchText(e.target.value);
+
+    setSearchedTimeOut(
+      setTimeout(() => {
+        const serach = filterProps(e.target.value);
+        setSearchedResults(serach);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (searchText) => {
+    setSearchText(searchText);
+
+    const serach = filterProps(searchText);
+    setSearchedResults(serach);
+  };
+
+  const filterProps = (searchText) => {
+    const reg = new RegExp(searchText, "i");
+    return allPosts.filter(
+      (item) =>
+        reg.test(item.creator.username) ||
+        reg.test(item.tag) ||
+        reg.test(item.prompt)
+    );
+  };
 
   const fetchPost = async () => {
     const response = await fetch("/api/prompt", { cache: "no-store" });
     const data = await response.json();
 
     setAllPosts(data);
+    setSearchedResults(data);
   };
 
   useEffect(() => {
@@ -41,10 +71,11 @@ const Feed = () => {
           onChange={handleSearchChange}
           required
           className="search_input peer"
+          value={searchText}
         ></input>
       </form>
 
-      <PromptCardList data={allPosts} handleTagClick={() => {}} />
+      <PromptCardList data={serchedResults} handleTagClick={handleTagClick} />
     </section>
   );
 };
